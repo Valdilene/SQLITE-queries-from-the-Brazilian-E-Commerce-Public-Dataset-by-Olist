330971f0babe09f90e46b9bfff4ed9e743cd4de5
Valdilene <76168739+Valdilene@users.noreply.github.com>
2023-06-14 10:03:06 +0200

    Update Tarefa1_1.sql

--- SQL/Tarefa1_1.sql
+++ SQL/Tarefa1_1.sql
@@ -2,10 +2,5 @@
 1. Selecione os dados da tabela de pagamentos onde só apareçam os tipos de pagamento “VOUCHER” e “BOLETO”.*/
 SELECT *
 FROM olist_order_payments_dataset
-WHERE payment_type = 'voucher' or payment_type = 'boleto'
+WHERE UPPER (TRIM (payment_type)) IN ('BOLETO', 'VOUCHER')
 ORDER BY payment_type ASC;
-
---Sol. alternativa
---SELECT *
---FROM olist_order_payments_dataset
---WHERE UPPER (TRIM (payment_type)) IN ('BOLETO', 'VOUCHER') -- UPPER capitalise all letters and TRIM delete blank spaces surrounding the words
